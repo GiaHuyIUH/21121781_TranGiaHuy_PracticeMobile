c2274222a7d4697e543e09fdefd2d7e3df97f46d
GiaHuyIUH <tranhuyzaza@gmail.com>
2024-09-04 23:02:49 +0700

    second screen

--- Buoi3/react-native/HelloWorld/app/index.js
+++ Buoi3/react-native/HelloWorld/app/index.js
@@ -1,65 +1,10 @@
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import SecondScreen from "./screens/SecondScreen";
 
 export default function Page() {
   return (
-    <View style={{ flex: 1, backgroundColor: "#00CCF9" }}>
-      <View
-        style={{ flex: 1.5, justifyContent: "center", alignItems: "center" }}
-      >
-        <View
-          style={{
-            height: 100,
-            width: 100,
-            borderRadius: 50,
-            borderColor: "#000",
-            borderWidth: "12px",
-          }}
-        ></View>
-      </View>
-      <View style={{ flex: 1 }}>
-        <Text style={styles.h1}>GROW</Text>
-        <Text style={styles.h1}>YOUR BUSINESS</Text>
-      </View>
-      <View style={{ flex: 1.5 }}>
-        <Text style={styles.text}>
-          We will help you to grow your business using
-        </Text>
-        <Text style={styles.text}>online server</Text>
-        <View
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-around",
-            marginTop: 40,
-          }}
-        >
-          <TouchableOpacity style={styles.btn}>
-            <Text>LOGIN</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.btn}>
-            <Text>SIGN UP</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    </View>
+    <>
+      <SecondScreen />
+    </>
   );
 }
-
-const styles = {
-  h1: {
-    fontWeight: 700,
-    fontSize: 20,
-    textAlign: "center",
-  },
-  text: {
-    fontWeight: 700,
-    textAlign: "center",
-  },
-  btn: {
-    backgroundColor: "yellow",
-    paddingHorizontal: 20,
-    borderRadius: "10px",
-    paddingVertical: 10,
-    fontWeight: 600,
-  },
-};
---
+++ Buoi3/react-native/HelloWorld/app/screens/SecondScreen.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+
+export default function SecondScreen() {
+  return (
+    <View style={{ flex: 1, backgroundColor: "#00CCF9" }}>
+      <View
+        style={{ flex: 1.5, justifyContent: "center", alignItems: "center" }}
+      >
+        <View
+          style={{
+            height: 100,
+            width: 100,
+            borderRadius: 50,
+            borderColor: "#000",
+            borderWidth: "12px",
+          }}
+        ></View>
+      </View>
+      <View style={{ flex: 1 }}>
+        <Text style={styles.h1}>GROW</Text>
+        <Text style={styles.h1}>YOUR BUSINESS</Text>
+      </View>
+      <View style={{ flex: 1.5 }}>
+        <Text style={styles.text}>
+          We will help you to grow your business using
+        </Text>
+        <Text style={styles.text}>online server</Text>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-around",
+            marginTop: "40px",
+          }}
+        >
+          <TouchableOpacity style={styles.btn}>
+            <Text>LOGIN</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btn}>
+            <Text>SIGN UP</Text>
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity style={{ marginTop: 20 }}>
+          <Text style={{ fontWeight: 650, textAlign: "center" }}>
+            HOW WE WORK?
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = {
+  h1: {
+    fontWeight: 700,
+    fontSize: 20,
+    textAlign: "center",
+  },
+  text: {
+    fontWeight: 700,
+    textAlign: "center",
+  },
+  btn: {
+    backgroundColor: "yellow",
+    paddingHorizontal: 20,
+    borderRadius: "10px",
+    paddingVertical: 10,
+    fontWeight: 600,
+  },
+};
