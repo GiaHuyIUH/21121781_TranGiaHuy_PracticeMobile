3b4bfde52c2b233134764c623a0288ea579efba9
H51M14\Student <Student@gmail.com>
2024-09-20 08:56:25 +0700

    Init Buoi 5

---
+++ Buoi5/react-native/Navigation/app/index.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import Screen1 from "./screens/Screen1";
+
+export default function Page() {
+  return (
+    <>
+    <Screen1 />
+    </>
+  );
+}
